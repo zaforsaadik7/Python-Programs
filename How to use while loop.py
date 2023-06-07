11a1e6180576b3c0587e517aca884b921e0152ed
zaforsaadik7 <121710926+zaforsaadik7@users.noreply.github.com>
2023-06-07 14:34:04 +0600

    Create How to use while loop.py

---
+++ How to use while loop.py
@@ -0,0 +1,17 @@
+x=0
+while x<=10:
+    print(x)
+    x+=1
+x=0
+while x<=15:
+    if x==10:
+        break
+        print('found !')
+    x+=1
+x=0
+while True:
+    print(x)
+    if x==10:
+        break
+    x+=1
+
