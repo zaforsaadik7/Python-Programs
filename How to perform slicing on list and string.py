58197784934a8fbc51f021f78ad021c7a0808c4e
zaforsaadik7 <121710926+zaforsaadik7@users.noreply.github.com>
2023-06-20 13:08:09 +0600

    Create How to perform slicing on list and string.py

---
+++ How to perform slicing on list and string.py
@@ -0,0 +1,16 @@
+my_list=[1,2,3,4,5,6,7,8,9]
+my_string='this is a string which says,my name is md emam zafor saadik'
+
+reverse_list=my_list[::-1]
+print(reverse_list)
+reverse_string=my_string[::-1]
+print(reverse_string)
+
+slice_list=my_list[:5:1]
+print(slice_list)
+even_numbers=my_list[1:-1:2]
+print(even_numbers)
+
+my_name=my_string[-31::1]
+print(my_name)
+
